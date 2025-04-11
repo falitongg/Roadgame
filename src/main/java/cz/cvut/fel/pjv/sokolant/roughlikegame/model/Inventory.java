f73f367c86bb5e2c5894c865ab605de7ee078bb4
test <sokolant@student.cvut.cz>
2025-04-12 00:39:59 +0200

    Inventory class extension

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Inventory.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Inventory.java
@@ -1,4 +1,34 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    private ArrayList<Item> items;
+
+    public Inventory() {
+        items = new ArrayList<>();
+    }
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public Item getItem(int index) {
+        if (index >= 0 && index < items.size()) {
+            return items.get(index);
+        }
+        return null;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+    public int getSize() {
+        return items.size();
+    }
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
 }
