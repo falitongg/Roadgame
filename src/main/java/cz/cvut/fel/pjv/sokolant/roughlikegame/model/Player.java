a779f7ca0cb9f2f0bc71963c25edb763f1f05ca7
test <sokolant@student.cvut.cz>
2025-04-11 14:54:46 +0200

    drobne prepracovani tridy Player. added position attributes

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
@@ -1,77 +1,100 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 
 public class Player {
-    private int health;     // Player's total health, dies at 0
-    private int stamina;    // Energy for running and fighting
-    private int thirst;     // Increases over time, affects health
-    private int hunger;     // Like starvation, but grows faster, also harmful
-    private int armor;      // Defense — reduces the damage received
-    private int radiation;  // Radiation level — causes damage over time
-    private int speed;      //Affects movement speed, may vary depending on the player's state (e.g. 0 stamina - temporary inability to move).
-//SETTERS
-    public void setHealth(int health) {
+    private float health;// Player's total health, dies at 0
+    private float stamina;// Energy for running and
+    private float thirst;// Increases over time, affects
+    private float hunger;// Like starvation, but grows faster, also harmful
+    private float armor;// Defense — reduces the damage received
+    private float radiation; // Radiation level — causes damage over time
+    private float speed;//Affects movement speed, may vary depending on the player's state (e.g. 0 stamina - temporary inability to move).
+    private float x;
+    private float y;
+
+    // === SETTERS ===
+    public void setHealth(float health) {
         this.health = health;
     }
 
-    public void setStamina(int stamina) {
+    public void setStamina(float stamina) {
         this.stamina = stamina;
     }
 
-    public void setThirst(int thirst) {
+    public void setThirst(float thirst) {
         this.thirst = thirst;
     }
 
-    public void setHunger(int hunger) {
+    public void setHunger(float hunger) {
         this.hunger = hunger;
     }
 
-    public void setArmor(int armor) {
+    public void setArmor(float armor) {
         this.armor = armor;
     }
 
-    public void setRadiation(int radiation) {
+    public void setRadiation(float radiation) {
         this.radiation = radiation;
     }
 
-    public void setSpeed(int speed) {
+    public void setSpeed(float speed) {
         this.speed = speed;
     }
-//GETTERS
-    public int getHealth() {
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    // === GETTERS ===
+    public float getHealth() {
         return health;
     }
 
-    public int getStamina() {
+    public float getStamina() {
         return stamina;
     }
 
-    public int getThirst() {
+    public float getThirst() {
         return thirst;
     }
 
-    public int getHunger() {
+    public float getHunger() {
         return hunger;
     }
 
-    public int getRadiation() {
+    public float getRadiation() {
         return radiation;
     }
 
-    public int getArmor() {
+    public float getArmor() {
         return armor;
     }
 
-    public int getSpeed() {
+    public float getSpeed() {
         return speed;
     }
-//CONSTRUCTOR
-    public Player(int health, int stamina, int thirst, int armor, int hunger, int radiation, int speed) {
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    // === CONSTRUCTOR ===
+    public Player(float health, float armor, float stamina, float thirst, float hunger, float radiation, float speed) {
         this.health = health;
+        this.armor = armor;
         this.stamina = stamina;
         this.thirst = thirst;
-        this.armor = armor;
         this.hunger = hunger;
         this.radiation = radiation;
         this.speed = speed;
+        this.x = 0;
+        this.y = 0;
     }
 }
