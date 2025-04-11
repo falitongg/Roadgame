ef1e04e06caf2a61df7c77621169204304e3dc7d
test <sokolant@student.cvut.cz>
2025-04-11 14:30:02 +0200

    added attributes

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
@@ -1,4 +1,11 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 
 public class Player {
+    private int health;
+    private int stamina;
+    private int thirst;
+    private int hunger;
+    private int armor;
+    private int radiation;
+    private int speed;
 }
