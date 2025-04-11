d9276bb9b617101c58adad10c953e49b99bc0181
test <sokolant@student.cvut.cz>
2025-04-11 15:38:21 +0200

    added getters to Item

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Item.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Item.java
@@ -1,4 +1,33 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 
+import cz.cvut.fel.pjv.sokolant.roughlikegame.util.ItemType;
+
 public class Item {
+    private String itemName;
+    private String itemDescription;
+    private ItemType itemType;
+    private float effectValue;
+
+    public Item(String itemName, String itemDescription, ItemType itemType, float effectValue) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemType = itemType;
+        this.effectValue = effectValue;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public ItemType getItemType() {
+        return itemType;
+    }
+
+    public float getEffectValue() {
+        return effectValue;
+    }
 }
