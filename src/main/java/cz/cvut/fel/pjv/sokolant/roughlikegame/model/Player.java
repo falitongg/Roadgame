6eb7544433a6ccd7738722e8a614398b62228b3b
test <sokolant@student.cvut.cz>
2025-04-11 14:35:00 +0200

    added comments

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
@@ -1,11 +1,12 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 
 public class Player {
-    private int health;
-    private int stamina;
-    private int thirst;
-    private int hunger;
-    private int armor;
-    private int radiation;
-    private int speed;
+    private int health;     // Player's total health, dies at 0
+    private int stamina;    // Energy for running and fighting
+    private int thirst;     // Increases over time, affects health
+    private int hunger;     // Like starvation, but grows faster, also harmful
+    private int armor;      // Gets from surroundings/enemies, deals damage
+    private int radiation;  // Defense - reduces the damage received
+    private int speed;      //Affects movement speed, may vary depending on the player's state (e.g. 0 stamina - temporary inability to move).
+
 }
