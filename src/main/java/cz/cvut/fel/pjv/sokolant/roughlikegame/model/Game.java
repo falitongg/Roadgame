18e12f9e0bbb1e32bb7c48fef0ac84194017445c
test <sokolant@student.cvut.cz>
2025-04-12 01:17:41 +0200

    Game class extension

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Game.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Game.java
@@ -1,4 +1,43 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
-}
+    private Player player;
+    private List<Enemy> enemies;
+    private boolean gameOver;
+
+    public Game() {
+        this.player = new Player();
+        this.enemies = new ArrayList<>();
+        this.gameOver = false;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public List<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+    public void startGame(){
+        //TODO логика инициализации мира, врагов и т. п
+    }
+    public void update() {
+        if (!player.isAlive()) {
+            endGame();
+        }
+        //TODO логика тиков
+    }
+    public void endGame() {
+        gameOver = true;
+    }
+    public void spawnEnemy(Enemy enemy) {
+        enemies.add(enemy);
+    }
+
+    }
