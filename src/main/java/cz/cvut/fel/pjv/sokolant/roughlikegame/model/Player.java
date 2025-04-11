1f5da573a28464e3c6c6ac79d8d26b8afbe63221
test <sokolant@student.cvut.cz>
2025-04-11 14:38:06 +0200

    added gettery a settery to Player

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Player.java
@@ -9,4 +9,59 @@ public class Player {
     private int radiation;  // Defense - reduces the damage received
     private int speed;      //Affects movement speed, may vary depending on the player's state (e.g. 0 stamina - temporary inability to move).
 
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public void setThirst(int thirst) {
+        this.thirst = thirst;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void setRadiation(int radiation) {
+        this.radiation = radiation;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getRadiation() {
+        return radiation;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
