032fa71823f14ef5a7f36f159fc0ffcac2205426
test <sokolant@student.cvut.cz>
2025-04-13 01:40:31 +0200

    Enemy class extended

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Enemy.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Enemy.java
@@ -2,55 +2,16 @@ package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 
 import cz.cvut.fel.pjv.sokolant.roughlikegame.util.EnemyType;
 
-public class Enemy {
-    private float health;
-    private float damage;
-    private float x;
-    private float y;
+public class Enemy extends Entity {
     private EnemyType type;
 
-    public Enemy(EnemyType type, float y, float x, float damage, float health) {
+    public Enemy(float x, float y, float health, float damage, EnemyType type) {
+        super(x, y, health, damage);
         this.type = type;
-        this.y = y;
-        this.x = x;
-        this.damage = damage;
-        this.health = health;
-    }
-
-    public float getHealth() {
-        return health;
-    }
-
-    public float getDamage() {
-        return damage;
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
     }
 
     public EnemyType getType() {
         return type;
-    }
-
-    public void setHealth(float health) {
-        this.health = health;
-    }
-
-    public void setDamage(float damage) {
-        this.damage = damage;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public void setY(float y) {
-        this.y = y;
     }
 
     public void setType(EnemyType type) {
@@ -69,10 +30,5 @@ public class Enemy {
     public void die() {
         // TODO: логика смерти врага
     }
-
-    public boolean isAlive() {
-        return health > 0;
-    }
-
 
 }
