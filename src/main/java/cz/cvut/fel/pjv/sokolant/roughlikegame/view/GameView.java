b90c4cf3ba5944f3c50cd6e829fb22ad3ee6af1d
test <sokolant@student.cvut.cz>
2025-04-12 14:53:36 +0200

    kostra GameView

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/view/GameView.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/view/GameView.java
@@ -1,12 +1,30 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.view;
 
 import javafx.application.Application;
+import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.StageStyle;
 
 public class GameView extends Application {
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Game View");
+        Pane pane = new Pane();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        pane.getChildren().add(canvas);
+
+        primaryStage.setScene(new Scene(pane, WIDTH, HEIGHT));
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
     public static void main(String[] args) {
