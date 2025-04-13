d76fb78468f1590bb81f5903cdf8717babbd7d9f
test <sokolant@student.cvut.cz>
2025-04-14 00:43:19 +0200

    added Main menu

---
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/Main.java
@@ -0,0 +1,23 @@
+package cz.cvut.fel.pjv.sokolant.roughlikegame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("designtest.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("ROAD");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,8 @@ module cz.cvut.fel.pjv.sokolant.roughlikegame {
 
     opens cz.cvut.fel.pjv.sokolant.roughlikegame.view to javafx.graphics;
     exports cz.cvut.fel.pjv.sokolant.roughlikegame;
+    exports cz.cvut.fel.pjv.sokolant.roughlikegame.controller;
+
+    opens cz.cvut.fel.pjv.sokolant.roughlikegame.controller to javafx.fxml;
+
 }
