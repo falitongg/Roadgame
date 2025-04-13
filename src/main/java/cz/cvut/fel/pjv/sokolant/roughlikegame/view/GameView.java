b9059ab3815102c34e9a2340eda23f2c7d8b3ebc
test <sokolant@student.cvut.cz>
2025-04-13 03:18:51 +0200

    sketches edited, added prehlednost a citelnost kodu

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/view/GameView.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/view/GameView.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.view;
 
+import cz.cvut.fel.pjv.sokolant.roughlikegame.model.Game;
 import javafx.application.Application;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
@@ -13,49 +14,67 @@ import javafx.animation.AnimationTimer;
 
 
 public class GameView extends Application {
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private Game game;
+    private Image background;
 
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
 
-    private Image background;
-    private Image player;
-
-    private double playerX = 100;
-    private double playerY = HEIGHT - 200;
-
-    private GraphicsContext gc;
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Game View");
+        initGame();
+        initUI(primaryStage);
+        startGameLoop();
+    }
 
-        Pane pane = new Pane();
+    private void initGame() {
+        game = new Game();
+        background = new Image(getClass().getResourceAsStream("/images/bgs/background_alpha.png"));
+    }
+
+
+    private void initUI(Stage primaryStage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
-        pane.getChildren().add(canvas);
+        gc = canvas.getGraphicsContext2D();
 
-        primaryStage.setScene(new Scene(pane, WIDTH, HEIGHT));
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Pane root = new Pane();
+        root.getChildren().add(canvas);
 
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
 
-        Image background = new Image(getClass().getResourceAsStream("/images/bgs/background_alpha.png"));
-        Image player = new Image(getClass().getResourceAsStream("/images/player/player1.png"));
-
-        playerX = 100;
-        playerY = HEIGHT - 200; // гарантированно не обрежет игрока
-
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                gc.clearRect(0, 0, WIDTH, HEIGHT); // очищаем всё
-                gc.drawImage(background, 0, 0, WIDTH, HEIGHT); // фон
-                gc.drawImage(player ,playerX, playerY, 200, 200); // игрок
-            }
-        }.start();
-
+        primaryStage.setTitle("Roughlike");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    private void startGameLoop() {
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                render();
+            }
+        };
+        timer.start();
+    }
+
+    private void render() {
+        gc.clearRect(0, 0, WIDTH, HEIGHT);
+        renderBackground();
+        renderEntities();
+    }
+    private void renderBackground() {
+        gc.drawImage(background, 0, 0, WIDTH, HEIGHT);
+    }
+    private void renderEntities() {
+        game.getPlayer().render(gc);
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
+
 }
+
