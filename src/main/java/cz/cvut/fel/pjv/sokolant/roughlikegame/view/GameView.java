5670fc8fffc8fa616726c0659183b8bef428fb08
test <sokolant@student.cvut.cz>
2025-04-13 02:26:31 +0200

    GameView extended

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/view/GameView.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/view/GameView.java
@@ -17,8 +17,13 @@ public class GameView extends Application {
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
 
-    private double playerX;
-    private double playerY;
+    private Image background;
+    private Image player;
+
+    private double playerX = 100;
+    private double playerY = HEIGHT - 200;
+
+    private GraphicsContext gc;
 
     @Override
     public void start(Stage primaryStage) {
