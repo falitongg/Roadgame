6ed4f96c5f1221da0e4fed034010ceae02b15578
test <sokolant@student.cvut.cz>
2025-04-13 02:00:10 +0200

    Game class extended

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Game.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Game.java
@@ -1,16 +1,18 @@
 package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
 import java.util.ArrayList;
 import java.util.List;
+import cz.cvut.fel.pjv.sokolant.roughlikegame.util.GameState;
+
 
 public class Game {
     private Player player;
     private List<Enemy> enemies;
-    private boolean gameOver;
+    private GameState currentState;
 
     public Game() {
         this.player = new Player();
         this.enemies = new ArrayList<>();
-        this.gameOver = false;
+        this.currentState = GameState.MENU;
     }
 
     public Player getPlayer() {
@@ -21,21 +23,27 @@ public class Game {
         return enemies;
     }
 
-    public boolean isGameOver() {
-        return gameOver;
-    }
     public void startGame(){
         //TODO логика инициализации мира, врагов и т. п
     }
     public void update() {
         if (!player.isAlive()) {
             endGame();
-        }
+        }if (currentState != GameState.PLAYING) return;
         //TODO логика тиков, проверка столкновений и урона от врагов
     }
-    public void endGame() {
-        gameOver = true;
+    public GameState getState() {
+        return currentState;
     }
+
+    public void setState(GameState state) {
+        this.currentState = state;
+    }
+
+    public void endGame() {
+        currentState = GameState.GAME_OVER;
+    }
+
     public void spawnEnemy(Enemy enemy) {
         enemies.add(enemy);
     }
