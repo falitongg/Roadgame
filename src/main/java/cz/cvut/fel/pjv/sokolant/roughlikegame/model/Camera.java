133179eec2e1046fb504723acb59bf96cfe80a38
test <sokolant@student.cvut.cz>
2025-04-27 14:14:20 +0200

    pridal jsem Camera class pro pohodlny pohyb kamery. pripravuju se pudu pro parallax

---
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/model/Camera.java
@@ -0,0 +1,23 @@
+package cz.cvut.fel.pjv.sokolant.roughlikegame.model;
+
+public class Camera {
+    private double cameraX;
+    private final double screenCenter;
+
+    public Camera(double screenCenter) {
+        this.screenCenter = screenCenter;
+        this.cameraX = 0;
+    }
+
+    public void update(double playerX) {
+        if (playerX > screenCenter) {
+            this.cameraX = playerX - screenCenter;
+        } else {
+            this.cameraX = 0;
+        }
+    }
+
+    public double getX() {
+        return cameraX;
+    }
+}
