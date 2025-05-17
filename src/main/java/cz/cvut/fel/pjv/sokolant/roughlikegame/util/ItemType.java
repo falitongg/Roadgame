f968edff76ccc001a8d5f3ee776e6a3758fac044
test <sokolant@student.cvut.cz>
2025-05-18 01:25:55 +0200

    javadoc prubezny commit 4

--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/util/ItemType.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/util/ItemType.java
@@ -3,22 +3,48 @@ package cz.cvut.fel.pjv.sokolant.roughlikegame.util;
 import cz.cvut.fel.pjv.sokolant.roughlikegame.model.Item;
 import javafx.scene.image.Image;
 
+/**
+ * Enumeration representing different types of usable or collectible items in the game.
+ * Includes utility methods for random loot generation and retrieving associated icons.
+ */
 public enum ItemType {
+
+    /** Restores player health (+50 HP). */
     BANDAGE,
+
+    /** Restores player stamina (+50 STAMINA). */
     WATER,
+
+    /** Adds armor to the player (+50 ARMOR). */
     ARMOR,
-//    KEYCARD,
+
+    /** Equippable knuckle weapon that increases damage. */
     BOXER,
+
+    /** Craftable item that boosts stamina and can extinguish fire. */
     BUCKET;
 
+    // Defines which item types can be randomly spawned as loot
     private static final ItemType[] spawnable = {
             BANDAGE, WATER, ARMOR
     };
 
-    public static ItemType getRandom(){
+    /**
+     * Returns a random lootable item type from the predefined set.
+     *
+     * @return randomly chosen ItemType
+     */
+    public static ItemType getRandom() {
         return spawnable[(int)(Math.random() * spawnable.length)];
     }
 
+    /**
+     * Returns the icon image associated with a specific item type.
+     * Only predefined types are supported.
+     *
+     * @param type the item type to fetch the image for
+     * @return Image representing the item, or null if unavailable
+     */
     public static Image getImage(ItemType type) {
         switch (type) {
             case BANDAGE: return new Image(Item.class.getResourceAsStream("/images/items/bandage.png"));
--- src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/util/LoggingUtil.java
+++ src/main/java/cz/cvut/fel/pjv/sokolant/roughlikegame/util/LoggingUtil.java
@@ -3,28 +3,49 @@ package cz.cvut.fel.pjv.sokolant.roughlikegame.util;
 import java.io.IOException;
 import java.util.logging.*;
 
+/**
+ * Utility class for configuring Java's global logging system.
+ * Allows enabling or disabling logging based on a system property.
+ * If logging is enabled, messages are written to a file named {@code log.txt}.
+ *
+ * <p>Usage:</p>
+ * <pre>{@code
+ *   LoggingUtil.setup();
+ * }</pre>
+ *
+ * <p>To disable logging entirely, pass the system property:
+ * {@code -DLOGGING=false} when launching the application.</p>
+ */
 public class LoggingUtil {
 
+    /**
+     * Sets up the global logger configuration.
+     * Logging is directed to a file {@code log.txt} if enabled,
+     * otherwise it is turned off completely.
+     *
+     * System property: {@code LOGGING=true|false}
+     */
     public static void setup() {
         Logger logger = Logger.getLogger("");
         logger.setUseParentHandlers(false);
 
-        for(Handler handler : logger.getHandlers()) {
+        for (Handler handler : logger.getHandlers()) {
             logger.removeHandler(handler);
         }
-        //logging off
+
+        // Check system property to determine if logging is enabled
         String flag = System.getProperty("LOGGING", "true");
-        if(!Boolean.parseBoolean(flag)) {
+        if (!Boolean.parseBoolean(flag)) {
             logger.setLevel(Level.OFF);
             return;
         }
 
-        //logging on
-        try{
+        // Set up file logging
+        try {
             FileHandler fh = new FileHandler("log.txt");
             fh.setFormatter(new SimpleFormatter());
             logger.addHandler(fh);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
